909b88b550887d4d1b1e1340a7d339ae788bcad5
siiky <siiky@users.noreply.github.com>
2022-08-14 00:08:09 +0100

    Add simple usage example

---
+++ example.scm
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S csi -s
+
+(load "alist-let.scm")
+
+(print "Fixnum keys")
+(alist-let '((0 . 1) (2 . 3) (4 . 5))
+           ((a 0) (b 2) (c 4) (d 6 42))
+           (print "a: " a)
+           (print "b: " b)
+           (print "c: " c)
+           (print "d: " d))
+(newline)
+
+(print "Symbol keys")
+(alist-let '((a . 1) (b . 3) (c . 5))
+           ((a 'a) (b 'b) (c 'c) (d 'd 42))
+           (print "a: " a)
+           (print "b: " b)
+           (print "c: " c)
+           (print "d: " d))
+(newline)
+
+(print "String keys")
+(alist-let '(("a" . 1) ("b" . 3) ("c" . 5))
+           ((a "a" #f string=?) (b "b" #f string=?) (c "c" #f string=?) (d "d" 42 string=?))
+           (print "a: " a)
+           (print "b: " b)
+           (print "c: " c)
+           (print "d: " d))
+(newline)
