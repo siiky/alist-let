9d182e7209fded8b1f0c1d5b965511490bb02676
siiky <siiky@users.noreply.github.com>
2022-08-14 00:16:18 +0100

    Better document `alist-let` and `alist-values`

--- alist-let.scm
+++ alist-let.scm
@@ -3,6 +3,13 @@
 ;;; Returns the values corresponding to keys of an alist, with optional default
 ;;; values.
 ;;;
+;;; (alist-values ALIST LVAR ...)
+;;;
+;;; LVAR ::= KEY
+;;;        | (KEY)
+;;;        | (KEY DEFAULT)
+;;;        | (KEY DEFAULT =?)
+;;;
 ;;; (alist-values '((foo . "foo") (bar . "bar") (zaz . "zaz")) 'foo 'zaz)
 ;;; ;=> (values "foo" "zaz")
 ;;;
@@ -21,29 +28,35 @@
 ;;;     expand to the final expression.
 (define-syntax alist-values
   (syntax-rules (quote quasiquote)
+    ; All inputs have a default value and equality test now, so they have to be
+    ; reversed.
     ((alist-values "add-defaults" tmp-list alist ())
      (alist-values "reverse" tmp-list alist ()))
 
+    ; Expressions like 'abc and `abc are matched as (quote abc) and (quasiquote abc)
     ((alist-values "add-defaults" tmp-list alist ((quote key) keys ...))
      (alist-values "add-defaults" tmp-list alist (((quote key)) keys ...)))
     ((alist-values "add-defaults" tmp-list alist ((quasiquote key) keys ...))
      (alist-values "add-defaults" tmp-list alist (((quasiquote key)) keys ...)))
 
+    ; This key has an explicit default value and equality test, just move it to
+    ; the tmp-list.
     ((alist-values "add-defaults" (tmp-list ...)                      alist ((key default equal?) keys ...))
      (alist-values "add-defaults" ((key default equal?) tmp-list ...) alist (keys ...)))
 
+    ; This key has an explicit default value but no equality test, use `equal?`.
     ((alist-values "add-defaults" tmp-list alist ((key default) keys ...))
      (alist-values "add-defaults" tmp-list alist ((key default equal?) keys ...)))
 
-    ; Hack to make the `alist-let` implementation simpler, so there's no need
-    ; to do the `add-defaults` phase in `alist-let` too.
+    ; Add a default value of #f if there's no explicit one.
     ((alist-values "add-defaults" tmp-list alist ((key) keys ...))
      (alist-values "add-defaults" tmp-list alist ((key #f) keys ...)))
 
     ; This clause is still necessary because alist-values may be used by
-    ; itself, without alist-let.
+    ; itself without alist-let.
     ((alist-values "add-defaults" tmp-list alist (key keys ...))
      (alist-values "add-defaults" tmp-list alist ((key #f) keys ...)))
+
 
     ((alist-values "reverse" () alist keys)
      (alist-values "result" alist keys))
@@ -51,14 +64,25 @@
     ((alist-values "reverse" (key tmp-list ...) alist (keys ...))
      (alist-values "reverse" (tmp-list ...)     alist (key keys ...)))
 
+
     ((alist-values "result" alist ((key default equal?) ...))
      (let ((%alist alist))
        ; TODO: How to implement this with better performance?
        (values (alist-ref key %alist equal? default) ...)))
 
+
     ((alist-values alist key ...)
      (alist-values "add-defaults" () alist (key ...)))))
 
+
+;;;
+;;; (alist-let ALIST (LVAR ...)
+;;;   BODY ...)
+;;;
+;;; LVAR ::= (VAR KEY)
+;;;        | (VAR KEY DEFAULT)
+;;;        | (VAR KEY DEFAULT =?)
+;;;
 (define-syntax alist-let
   (syntax-rules ()
     ((alist-let alist ((var key opts ...) ...)
